76ba46ad62383de4f8474ba44063d675aeab3872
17crown1901mituru <17crown1901@gmail.com>
2026-07-25 04:10:55 +0900

    Sync: 2026-07-25 04:10:55

--- app/src/main/java/com/B/b/Renderer/js/JsDocument.kt
+++ app/src/main/java/com/B/b/Renderer/js/JsDocument.kt
@@ -1,6 +1,7 @@
 package com.B.b.Renderer.js
 
 import com.B.b.Renderer.core.Element
+import org.mozilla.javascript.Function
 
 class JsDocument(
     private val root: Element,
@@ -31,4 +32,30 @@ class JsDocument(
 
     val body: JsElement
         get() = (root.findFirst { it.tag == "body" } ?: root).let { registry.wrap(it) }
+
+    /**
+     * document直下ラッパー。実DOMのdocumentノードに相当するものをこのエンジンは
+     * 持たないため、ツリーのルート要素(root)を「document相当」として使い回す。
+     * root.findFirst{tag=="body"}ではなくrootそのものに委譲することで、
+     * <body>より外側(仮にあれば)で発火したイベントも拾える。
+     */
+    private val documentEventTarget: JsElement by lazy { registry.wrap(root) }
+
+    /**
+     * document.addEventListener(type, callback)相当。
+     * htmx.jsが起動時に多用する(例: click委譲、DOMContentLoaded相当のフック)。
+     * 実際にはrootに登録されるため、子要素で発火したイベントがdispatchEvent()の
+     * バブリングでrootまで届いた時点で発火する(実DOMのdocumentレベルリスナーと
+     * ほぼ同じ挙動)。
+     */
+    fun addEventListener(type: String, callback: Function) {
+        documentEventTarget.addEventListener(type, callback)
+    }
+
+    fun removeEventListener(type: String, callback: Function) {
+        documentEventTarget.removeEventListener(type, callback)
+    }
+
+    /** document.dispatchEvent(event)相当。root起点でdispatchする。 */
+    fun dispatchEvent(eventArg: Any?): Boolean = documentEventTarget.dispatchEvent(eventArg)
 }
